49f7c1737baa3fb704152347aa850244841e4dfa
finnerjones <finnseamus@gmail.com>
2014-09-16 10:50:10 +0200

    add an index with Java API using JSON

--- cg-poc-search/src/main/java/com/finnerjones/search/es/ElasticsearchConnection.java
+++ cg-poc-search/src/main/java/com/finnerjones/search/es/ElasticsearchConnection.java
@@ -7,6 +7,7 @@ package com.finnerjones.search.es;
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
@@ -15,15 +16,64 @@ import static org.elasticsearch.node.NodeBuilder.*;
 
 public class ElasticsearchConnection {
 
+    private Client client;
+    private Settings settings;
+
+
     public static void main(String[] args) {
+        ElasticsearchConnection conn = new ElasticsearchConnection();
+        conn.createClient();
+        conn.createSettings();
+        conn.doGet(conn.getClusterName());
+        conn.putIndexAsJSON();
+        conn.closeClient();
+    }
+
+    public void createClient() {
         Node node = nodeBuilder().node();
-        Client client = node.client();
-        Settings settings = client.settings();
-        String clustername = settings.get("cluster.name");
-        System.out.println("cluster.name: " + clustername);
-        GetRequest req = new GetRequest(clustername);
-        ActionFuture<GetResponse> resp =  client.get(req);
-        System.out.println(resp);
+        client = node.client();
+    }
+
+    public void createSettings() {
+        settings = client.settings();
+    }
+
+    public String getClusterName() {
+        return settings.get("cluster.name");
+    }
+
+    public ActionFuture<GetResponse> doGet(String clusterName) {
+        GetRequest req = new GetRequest(clusterName);
+        return client.get(req);
+    }
+
+
+    public void putIndexAsJSON() {
+        String json = "{" +
+            "\"user\":\"kimchy\"," +
+            "\"postDate\":\"2013-01-30\"," +
+            "\"message\":\"trying out Elasticsearch\"" +
+            "}";
+
+        IndexResponse response = client.prepareIndex("twitter", "tweet")
+                .setSource(json)
+                .execute()
+                .actionGet();
+
+        // Index name
+        String index = response.getIndex();
+        // Type name
+        String type = response.getType();
+        // Document ID
+        String id = response.getId();
+        // Version
+        long version = response.getVersion();
+
+        System.out.println("Index: " + index + "\n" + "Type: " + type + "\n" + "ID: " + id + "\n" + "Version: " + version);
+    }
+
+
+    public void closeClient() {
         client.close();
     }
 
